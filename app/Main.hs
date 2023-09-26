056174ddff7750e6e5feb8915aa53df6993ea067
Tianyu Chen <chentianyu@outlook.com>
2023-09-26 00:30:15 -0400

    parsing canvas template

--- app/Main.hs
+++ app/Main.hs
@@ -9,14 +9,14 @@ import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector          as V
 import GHC.Generics
 import Data.Csv
-import Text.Email.Validate
+-- import Text.Email.Validate
 
 data Student where
   Student :: {uid :: String, grade :: Float} -> Student
   deriving (Show, Eq, Generic)
 
 data GradeBookEntry where
-  GradeBookEntry :: {student :: Student, name :: String, sid :: Integer, section :: String} -> GradeBookEntry
+  GradeBookEntry :: {username :: Maybe String, name :: Maybe String, sid :: Maybe Integer, section :: Maybe String, pts :: Float} -> GradeBookEntry
   deriving (Show, Eq, Generic)
 
 instance FromRecord Student
@@ -28,19 +28,28 @@ instance FromNamedRecord Student where
       <$> r .: "Username"
       <*> r .: "Total Points"
 
+instance FromNamedRecord GradeBookEntry where
+  parseNamedRecord r =
+    GradeBookEntry
+    <$> r .: "SIS Login ID"
+    <*> r .: "Student"
+    <*> r .: "ID"
+    <*> r .: "Section"
+    <*> return 0
+
 instance ToNamedRecord GradeBookEntry where
   toNamedRecord :: GradeBookEntry -> NamedRecord
-  toNamedRecord (GradeBookEntry s n si sc) =
+  toNamedRecord (GradeBookEntry un n si sc pt) =
     namedRecord [
     "Student" .= n,
     "ID" .= si,
-    "SIS Login ID" .= uid s,
-    "Section" .= sc
+    "SIS Login ID" .= un,
+    "Section" .= sc,
+    "Grade" .= pt
     ]
 
--- Parse Autograder CSV file
-parseAutograder :: BL.ByteString -> V.Vector Student
-parseAutograder s =
+parse :: FromNamedRecord a => BL.ByteString -> V.Vector a
+parse s =
   case decodeByName s of
     Left  _       -> V.empty
     Right (_ , v) -> v
@@ -48,7 +57,15 @@ parseAutograder s =
 main :: IO()
 main =
   do
-    csvData <- BL.readFile "in.csv"
-    do V.forM_ (parseAutograder csvData) $
+    autograderCSVData <- BL.readFile "in.csv"
+    do V.forM_ (parse autograderCSVData) $
          \ p ->
            putStrLn $ uid p ++ " scores " ++ show (grade p)
+    templateData <- BL.readFile "template.csv"
+    do V.forM_ (parse templateData) $
+         \ p ->
+           putStrLn $ show (username p) ++ " scores " ++ show (pts p)
+    -- f <- BL.readFile "template.csv"
+    -- case decodeByName f of
+    --     Left err      -> print err
+    --     Right (_, xs) -> V.forM_ xs $ \(GradeBookEntry n _ _ _ _) -> print n
